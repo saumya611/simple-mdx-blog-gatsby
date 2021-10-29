abfc28ea5dda337e394ff849c301d3cc798bb13f
saumya611 <55658827+saumya611@users.noreply.github.com>
2021-10-29 17:00:32 +0530

    Added global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import Theme from './src/themes/theme';
+
+// In below line we are going to set some
+// CSS property for global means at root browser
+
+const GlobalStyles = createGlobalStyle`
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+        height: 100%;
+        background-color: ${props => props.theme.colors.light1};
+    }
+`
 
 export const wrapRootElement  = ({ element }) => (
     <ThemeProvider theme={Theme}>
+        <GlobalStyles />
         {element}
     </ThemeProvider>
 )
