03c517ac37d9fbcef9a19c507652a2db265b70df
saumya611 <55658827+saumya611@users.noreply.github.com>
2021-10-30 13:16:24 +0530

    Added nav

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { ContainerWrapper } from "../elements";
+import { Nav } from "../components/Nav";
 
 export const Container = ({ children }) => {
     return (
         <ContainerWrapper>
+            <Nav />
             {children}
         </ContainerWrapper>
     );
